a4be76438c20ea10c52f8fcd4f4394ddd6000a8f
Sam Zhang <samzhangjy@outlook.com>
2022-12-23 15:22:41 +0800

    chore: initial structure

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,61 @@
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import dts from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import { babel } from "@rollup/plugin-babel";
+
+/**
+ * @type {import('@rollup/plugin-babel').RollupBabelInputPluginOptions}
+ */
+const babelConfig = {
+  babelHelpers: "bundled",
+  extensions: [".js", ".ts"],
+};
+
+const commonPlugins = [nodeResolve({ browser: true }), commonjs(), babel(babelConfig)];
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default [
+  {
+    input: "./src/index.ts",
+    output: {
+      format: "esm",
+      sourcemap: true,
+      file: "./dist/newcar.mjs",
+    },
+    plugins: [typescript(), ...commonPlugins],
+    treeshake: true,
+  },
+  {
+    input: "./src/index.ts",
+    output: {
+      format: "es",
+      sourcemap: true,
+      name: "leaf",
+      file: "./dist/newcar.min.js",
+    },
+    plugins: [typescript(), ...commonPlugins, terser({ ecma: 2015 })],
+    treeshake: true,
+  },
+  {
+    input: "./src/index.ts",
+    output: {
+      format: "es",
+      sourcemap: true,
+      dir: "./dist/es",
+      preserveModules: true,
+    },
+    plugins: [typescript({ outputToFilesystem: false, outDir: "./dist/es" }), ...commonPlugins],
+  },
+  {
+    input: "./dist/src/index.d.ts",
+    output: {
+      file: "./dist/newcar.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
+  },
+];
---
+++ packages/core/src/index.ts
@@ -0,0 +1,3 @@
+export const hello = (name: string) => {
+  console.log(`Hello ${name}.`);
+};
