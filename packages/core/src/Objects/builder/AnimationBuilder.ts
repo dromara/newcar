48bcc249832c29bb66992718c21bce937f3c1dd1
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-12-28 10:48:51 +0800

    [feat] implemented AnimationBuilder.playOnCar(), but not tested

--- packages/core/src/Objects/builder/AnimationBuilder.ts
+++ packages/core/src/Objects/builder/AnimationBuilder.ts
@@ -5,10 +5,46 @@ export class AnimationBuilder {
   
   #items: AnimationBuilderItem[];
 
-  // TODO: Implement this method.
+  // TODO: Implemented, but not tested.
   playOnCar(carInstance: Car) {
-    this.#items.sort((a, b) => a.startFrame - b.startFrame);
-    carInstance.forEvery(frame => {});
+    let itemsClone = [...this.#items];
+    itemsClone.sort((a, b) => a.startFrame - b.startFrame);
+    carInstance.forEvery(frame => {
+      let sliceCounter = 0;
+      for (let i of itemsClone) {
+        
+        // Now hold on.
+        // This is a very simple algorithm.
+        // If you can't understand it, you are just fool.
+        // If you can understand it, you are fool, too.
+        //                                      ---- 27Onion
+        if (frame >= i.startFrame + i.length) {
+          // Out-dated.
+          sliceCounter++;
+          continue;
+        }
+
+        if (frame < i.startFrame) {
+          // You are not old enough to join this *activity*, bro
+          break;
+        }
+
+        // M⚡️U⚡️L⚡️T⚡️I⚡️P⚡️L⚡️A⚡️Y⚡️E⚡️R⚡️-⚡️S⚡️P⚡️O⚡️R⚡️T⚡️S
+        i.onDrawFrame(frame - i.startFrame, this);
+
+      }
+      itemsClone.slice(sliceCounter);
+    });
+  }
+
+  /**
+   * Add an animation builder iteme
+   * @param builderItem The builder item.
+   * @returns The reference to the builder itself.
+   */
+  addItem(builderItem: AnimationBuilderItem) : AnimationBuilder {
+    this.#items.push(builderItem);
+    return this;
   }
 
 }
