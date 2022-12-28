dedd21ba83d58e974cc978aaf023619cafc8daf0
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-12-29 07:54:08 +0800

    [chore] formatting NonlinearInterpolators

--- packages/core/src/Objects/interpolation/NonlinearInterpolators.ts
+++ packages/core/src/Objects/interpolation/NonlinearInterpolators.ts
@@ -1,5 +1,6 @@
+/* eslint-disable indent */
 export function EaseInSineInterpolator(x: number): number {
-    return 1 - Math.cos((x * Math.PI) / 2);
+  return 1 - Math.cos((x * Math.PI) / 2);
 }
 
 export function EaseOutSineInterpolator(x: number): number {
@@ -69,10 +70,11 @@ export function EaseOutExpoInterpolator(x: number): number {
 export function EaseInOutExpoInterpolator(x: number): number {
   return x === 0
     ? 0
-      : x === 1
-        ? 1
-          : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2
-            : (2 - Math.pow(2, -20 * x + 10)) / 2;
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? Math.pow(2, 20 * x - 10) / 2
+    : (2 - Math.pow(2, -20 * x + 10)) / 2;
 }
 
 export function EaseInCircInterpolator(x: number): number {
@@ -86,7 +88,7 @@ export function EaseOutCircInterpolator(x: number): number {
 export function EaseInOutCircInterpolator(x: number): number {
   return x < 0.5
     ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2
-      : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
+    : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2;
 }
 
 export function EaseInBackInterpolator(x: number): number {
@@ -109,27 +111,19 @@ export function EaseInOutBackInterpolator(x: number): number {
 
   return x < 0.5
     ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-      : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+    : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
 }
 
 export function EaseInElasticInterpolator(x: number): number {
   const c4 = (2 * Math.PI) / 3;
 
-  return x === 0
-    ? 0
-      : x === 1
-        ? 1
-          : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
+  return x === 0 ? 0 : x === 1 ? 1 : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4);
 }
 
 export function EaseOutElasticInterpolator(x: number): number {
   const c4 = (2 * Math.PI) / 3;
 
-  return x === 0
-    ? 0
-      : x === 1
-        ? 1
-          : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+  return x === 0 ? 0 : x === 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
 }
 
 export function EaseInOutElasticInterpolator(x: number): number {
@@ -137,11 +131,11 @@ export function EaseInOutElasticInterpolator(x: number): number {
 
   return x === 0
     ? 0
-      : x === 1
-        ? 1
-          : x < 0.5
-            ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
-              : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
+    : x === 1
+    ? 1
+    : x < 0.5
+    ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2
+    : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1;
 }
 
 export function EaseInBounceInterpolator(x: number): number {
@@ -153,18 +147,18 @@ export function EaseOutBounceInterpolator(x: number): number {
   const d1 = 2.75;
 
   if (x < 1 / d1) {
-      return n1 * x * x;
-      } else if (x < 2 / d1) {
-          return n1 * (x -= 1.5 / d1) * x + 0.75;
-          } else if (x < 2.5 / d1) {
-              return n1 * (x -= 2.25 / d1) * x + 0.9375;
-              } else {
-                  return n1 * (x -= 2.625 / d1) * x + 0.984375;
-                  }
+    return n1 * x * x;
+  } else if (x < 2 / d1) {
+    return n1 * (x -= 1.5 / d1) * x + 0.75;
+  } else if (x < 2.5 / d1) {
+    return n1 * (x -= 2.25 / d1) * x + 0.9375;
+  } else {
+    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+  }
 }
 
 export function EaseInOutBounceInterpolator(x: number): number {
   return x < 0.5
     ? (1 - EaseOutBounceInterpolator(1 - 2 * x)) / 2
-      : (1 + EaseOutBounceInterpolator(2 * x - 1)) / 2;
+    : (1 + EaseOutBounceInterpolator(2 * x - 1)) / 2;
 }
