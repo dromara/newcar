91614104fdd6d653d1be2ac7dc30248919593c5d
Acbox <850625057@qq.com>
2023-01-22 14:35:20 +0800

    [release] v0.0.0

--- packages/core/src/Objects/graph/lineSegment.ts
+++ packages/core/src/Objects/graph/lineSegment.ts
@@ -13,8 +13,10 @@ export class LineSegment extends Line {
     //   (this.primaryPoints[0].y / this.primaryPoints[1].y);
     // const displacement = this.primaryPoints[0].y - this.primaryPoints[0].x * slope;
     // const lineFunction = (x) => slope * x + displacement;
+    console.log("Start draw.");
     ctx.beginPath();
     ctx.moveTo(this.primaryPoints[0].x, this.primaryPoints[0].y);
     ctx.lineTo(this.primaryPoints[1].x, this.primaryPoints[1].y);
+    console.log("Compelet draw.");
   }
 }
