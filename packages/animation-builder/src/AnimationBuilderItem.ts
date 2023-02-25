4ac510b4c6bab6fe794e3c3421ea716600eaabf7
Acbox <850625057@qq.com>
2023-02-25 14:16:41 +0800

    Revert "[build]Re-build the package `@newcar/animation-builder`"
    This reverts commit 49b48c9a07ae3c298929e7df643bbc4bc3d1459d.

--- packages/animation-builder/src/AnimationBuilderItem.ts
+++ packages/animation-builder/src/AnimationBuilderItem.ts
@@ -1,27 +1,29 @@
 import { AnimationBuilder } from "./AnimationBuilder";
 import { IRenderable } from "@newcar/objects/src/interfaces/Renderable";
 import { IRendererController } from "@newcar/objects/src/interfaces/RenderController";
-
 export abstract class AnimationBuilderItem {
-  // /**
-  //  * Will be called on registration.
-  //  * @param parent The parent object.
-  //  */
-  // // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // onRegister<T extends IRenderable & IRendererController>(_carInstance: T): void {
-  //   return;
-  // }
-  // /**
-  //  * Will be called on drawing frame.
-  //  * @param relativeFrameCount The frame count relative to `fstart`.
-  //  */
-  // abstract onDrawFrame(relativeFrameCount: number, parent: AnimationBuilder): void;
-  // /**
-  //  * Decide when the animation starts.
-  //  */
-  // abstract get startFrame(): number;
-  // /**
-  //  * Decide how long the animation lasts.
-  //  */
-  // abstract get length(): number;
+  /**
+   * Will be called on registration.
+   * @param parent The parent object.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onRegister<T extends IRenderable & IRendererController>(_carInstance: T): void {
+    return;
+  }
+
+  /**
+   * Will be called on drawing frame.
+   * @param relativeFrameCount The frame count relative to `fstart`.
+   */
+  abstract onDrawFrame(relativeFrameCount: number, parent: AnimationBuilder): void;
+
+  /**
+   * Decide when the animation starts.
+   */
+  abstract get startFrame(): number;
+
+  /**
+   * Decide how long the animation lasts.
+   */
+  abstract get length(): number;
 }
