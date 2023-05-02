08433882e3c4f1c3e877507e6b82cb785f24478a
Acbox <850625057@qq.com>
2023-05-02 16:55:39 +0800

    [package]Add package `@newcar/export`

---
+++ packages/export/src/index.ts
@@ -0,0 +1,8 @@
+import { IRenderable } from "@newcar/objects/src/interfaces/Renderable";
+import { IRendererController } from "@newcar/objects/src/interfaces/RenderController";
+
+export const exportAnimation = (
+  core: IRenderable & IRendererController,
+  startAt: number,
+  lastAt: number
+) => {};
