99483482c681dd7753102eed2f3f6dc6aba0b85d
27Onion Nebell <zzy20080201@qq.com>
2023-08-30 21:18:26 +0800

    feat: Added hsl support for the `Color` class.

--- packages/core/src/color.ts
+++ packages/core/src/color.ts
@@ -31,6 +31,23 @@ export class Color {
     return result;
   }
 
+  static hsl(h: number, s: number, l: number) {
+    const ss = s / 100;
+    const ll = l / 100;
+    const k = (n: number) => (n + h / 30) % 12;
+    const a = ss * Math.min(ll, 1 - ll);
+    const f = (n: number) => ll - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+
+    return Color.rgb(255 * f(0), 255 * f(8), 255 * f(4));
+  }
+
+  static hsla(h: number, s: number, l: number, a: number) {
+    const result = Color.hsl(h, s, l);
+    result.a = a;
+
+    return result;
+  }
+
   toString() {
     return `rgba(${this.r}, ${this.g}, ${this.b}, ${this.a})`;
   }
