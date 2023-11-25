f0b3a873e0030bbc6669cf4b0e5283de9935d761
montmorill <montmorillonite@qq.com>
2023-11-25 22:26:19 +0800

    chore(objects): rename `start/end` to `from/to`

--- packages/basic/src/objects/figures/circle.ts
+++ packages/basic/src/objects/figures/circle.ts
@@ -3,14 +3,14 @@ import { Figure } from "./figure";
 
 /**
  * The circle options.
- * @param startAngle The start angle of the circle.
- * @param endAngle The end angle of the circle.
+ * @param from The start angle of the circle.
+ * @param to The end angle of the circle.
  * @see FigureOption
  * @see Circle
  */
 export interface CircleOption extends FigureOption {
-  startAngle?: number;
-  endAngle?: number;
+  from?: number;
+  to?: number;
 }
 
 /**
@@ -18,8 +18,8 @@ export interface CircleOption extends FigureOption {
  */
 export class Circle extends Figure implements CircleOption {
   radius: number;
-  startAngle: number;
-  endAngle: number;
+  from: number;
+  to: number;
 
   /**
    * @param radius The radius of the circle.
@@ -29,14 +29,14 @@ export class Circle extends Figure implements CircleOption {
   constructor(radius: number, options?: CircleOption) {
     super((options ??= {}));
     this.radius = radius;
-    this.startAngle = options.startAngle ?? 0;
-    this.endAngle = options.endAngle ?? 2 * Math.PI;
+    this.from = options.from ?? 0;
+    this.to = options.to ?? 2 * Math.PI;
   }
 
   override draw(context: CanvasRenderingContext2D): void {
     context.lineWidth = this.borderWidth;
     context.beginPath();
-    context.arc(0, 0, this.radius, this.startAngle, this.endAngle);
+    context.arc(0, 0, this.radius, this.from, this.to);
     if (this.fillColor) {
       context.fillStyle = this.fillColor.toString();
       context.fill();
