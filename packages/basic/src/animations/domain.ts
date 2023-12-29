1fb0255935c5ec09a876cf189e9cbae40b184446
Acbox <850625057@qq.com>
2023-12-29 23:56:21 +0800

    fix: Position was controled by animation domain.

--- packages/basic/src/animations/domain.ts
+++ packages/basic/src/animations/domain.ts
@@ -20,6 +20,6 @@ export const functionDomain: Animate = (
   params.fromEnd ??= params.from ?? object.domain[1];
   params.toStart ??= params.to ?? object.domain[0];
   params.toEnd ??= params.to ?? object.domain[1];
-  object.x = interpolator(params.fromStart, params.toStart, by)(process);
-  object.y = interpolator(params.fromEnd, params.toEnd, by)(process);
+  object.domain[0] = interpolator(params.fromStart, params.toStart, by)(process);
+  object.domain[1] = interpolator(params.fromEnd, params.toEnd, by)(process);
 };
