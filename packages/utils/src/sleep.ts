2df57b871ec555c0457f9c5c93df6dd6292cf9b8
Acbox <850625057@qq.com>
2024-02-03 14:31:41 +0800

    feat: Add full-css color support.

--- packages/utils/src/sleep.ts
+++ packages/utils/src/sleep.ts
@@ -1,7 +1,8 @@
 import { config } from "./config";
 
-export const sleep = (length: number): Promise<void> => {
+export function sleep(length: number): Promise<void> {
   const sleepTime =
     config.timing === "frame" ? length * (1000 / 60) : 1000 * length;
+
   return new Promise((resolve) => setTimeout(resolve, sleepTime));
-};
+}
