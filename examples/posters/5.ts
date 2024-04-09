6fbf192dece9faef97494a815ee551927225cdfe
Acbox <850625057@qq.com>
2024-04-09 19:28:00 +0800

    docs: update posters.

--- examples/posters/5.ts
+++ examples/posters/5.ts
@@ -9,16 +9,15 @@ const app = engine.createApp(document.querySelector('#poster'))
 const rcd = new nc.Recorder(document.querySelector('#poster'), 'mp4')
 
 const root = new nc.Widget().add(
-  new nc.Text("Hello, MY Newcar!", 'https://storage.googleapis.com/skia-cdn/misc/Roboto-Regular.ttf', {
-    y: 60,
-    x: 10,
-    style: {
-      size: 90
-    },
-  }).animate(nc.create, 0, 60)
+  new nc.Rect([0, 0], [100, 100], {
+    x: 350,
+    y: 175
+  }).add(
+    new nc.Arrow([0, 0], [0, 100]).animate(nc.create, 0, 30)
+  ).add(new nc.Widget().add(new nc.Arrow([0, 0], [0, -100]).animate(nc.create, 0, 30)))
 ).setUpdate((e, w) => {
-  if (e === 10) {
-    rcd.start(1000, (url) => {
+  if (e === 20) {
+    rcd.start(500, (url) => {
       console.log(url)
     })
   }
