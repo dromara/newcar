2ab5a755110f38458279459cd0707233b9a529d9
Acbox <850625057@qq.com>
2024-05-05 13:06:09 +0800

    docs: format

--- docs/content/.vitepress/theme/components/Demo3.vue
+++ docs/content/.vitepress/theme/components/Demo3.vue
@@ -1,40 +1,41 @@
-<template>
-  <canvas
-    ref="ncc3"
-    width="1600"
-    height="900"
-    id="canvas3"
-    style="width: 100%"
-    @click="app.play"
-  ></canvas>
-</template>
-
 <script setup lang="ts">
-import { Widget, create, App, CarEngine, Scene } from "newcar";
-import { NumberPlane, MathFunction } from "@newcar/mod-math";
-import { onMounted, ref } from "vue";
+import type { App } from 'newcar'
+import { CarEngine, Scene, Widget, create } from 'newcar'
+import { MathFunction, NumberPlane } from '@newcar/mod-math'
+import { onMounted, ref } from 'vue'
 
-let app: App;
+let app: App
 
-const ncc3 = ref<HTMLCanvasElement>();
+const ncc3 = ref<HTMLCanvasElement>()
 
 onMounted(() => {
   new CarEngine()
-    .init("https://unpkg.com/canvaskit-wasm@latest/bin/canvaskit.wasm")
+    .init('https://unpkg.com/canvaskit-wasm@latest/bin/canvaskit.wasm')
     .then((engine) => {
       if (ncc3.value) {
-        const app = engine.createApp(ncc3.value);
+        const app = engine.createApp(ncc3.value)
         const root = new Widget().add(
           new NumberPlane(-200, 200, -200, 200, {
             x: 400,
-            y: 225
-          })
+            y: 225,
+          }),
           // .add(new MathFunction(Math.sin, [-4, 4]).animate(create, 20, 80))
           // .add(new MathFunction(Math.cos, [-4, 4]).animate(create, 20, 80)),
-        );
-        const scene = new Scene(root);
-        app.checkout(scene);
+        )
+        const scene = new Scene(root)
+        app.checkout(scene)
       }
-    });
-});
+    })
+})
 </script>
+
+<template>
+  <canvas
+    id="canvas3"
+    ref="ncc3"
+    width="1600"
+    height="900"
+    style="width: 100%"
+    @click="app.play"
+  />
+</template>
