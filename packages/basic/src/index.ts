ad3efacd11666809537cd9f026bde40ca424f05c
Acbox <850625057@qq.com>
2024-05-07 10:05:06 +0800

    release: v1.0.0-alpha.7

--- packages/basic/src/index.ts
+++ packages/basic/src/index.ts
@@ -3,6 +3,4 @@ export * from './animations'
 export * from './events'
 
 export * from './utils/vector2'
-export * from '../../core/src/utils/trans'
-export * from '../../core/src/utils/types'
 export * from './easingFunctions'
