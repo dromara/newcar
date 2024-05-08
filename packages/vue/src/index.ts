3d2b1d9e144014e516bb5b2bd64a0051b575dd23
Acbox <850625057@qq.com>
2024-05-08 21:09:18 +0800

    docs: add typedoc for project

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,4 +1,4 @@
-import CarCanvas from './components/CarCanvas.vue'
+// import CarCanvas from './components/CarCanvas.vue'
 // export * from './hooks/useCarApp'
 export { newcar } from './plugin'
-export { CarCanvas }
+// export { CarCanvas }
