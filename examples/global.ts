ef8cf4e9ff65da29203101aa84385d91b9da02cd
Hydrogen <168127639+justyukie@users.noreply.github.com>
2024-05-12 17:44:01 +0800

    fix: disable example

--- examples/global.ts
+++ examples/global.ts
@@ -2,11 +2,11 @@
 // @ts-nocheck
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-/// <reference types="@newcar/mod-web-components/global" />
+//// <reference types="@newcar/mod-web-components/global" />
 
-const engine = await newcar.whenReady()
+// const engine = await newcar.whenReady()
 
-console.log(engine, newcar.getCanvasByName('foo'))
+// console.log(engine, newcar.getCanvasByName('foo'))
 
-export {}
+// export {}
 
