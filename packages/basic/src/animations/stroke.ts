a2b7b80bb9cbe7a32a542fe9c31276dcc17a9dd9
Acbox <850625057@qq.com>
2024-05-20 16:19:09 +0800

    fix: build failed

--- packages/basic/src/animations/stroke.ts
+++ packages/basic/src/animations/stroke.ts
@@ -1,11 +1,12 @@
 import { defineAnimation } from '@newcar/core'
+import type { CanvasKit } from 'canvaskit-wasm'
 import type { Figure } from '../widgets/figures/figure'
 
 /**
  * Stroke animation
  */
 export const stroke = defineAnimation({
-  act: (widget: Figure, elapsed: number, process: number, params: {
+  act: (widget: Figure, elapsed: number, process: number, ck: CanvasKit, params: {
     origin: number
   }) => {
     widget.style.interval = [(params.origin ?? 1000) * process, (params.origin ?? 1000) * (1 - process)]
