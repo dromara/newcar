543b9e02801fca7a34e934d15cd620f45abba377
Steven Xiang <wgxh-cli@qq.com>
2024-06-01 15:45:27 +0800

    refactor: Add `colorAttr` for `discolorate` to support more color animating

--- packages/basic/src/animations/discolorate.ts
+++ packages/basic/src/animations/discolorate.ts
@@ -1,5 +1,5 @@
 import { changeProperty, defineAnimation } from '@newcar/core'
-import type { Color } from '@newcar/utils'
+import { type Color, getByChain } from '@newcar/utils'
 import type { Figure } from '../widgets'
 
 /**
@@ -9,19 +9,21 @@ export const discolorate = defineAnimation<Figure>({
   init(widget, startAt, duration, _ck, params: {
     from?: Color
     to?: Color
+    colorAttr?: string
   }) {
+    const colorAttr: string = params.colorAttr ?? 'fillColor'
     const changedProps = [
-      'style.fillColor.red',
-      'style.fillColor.green',
-      'style.fillColor.blue',
-      'style.fillColor.alpha',
+      [`style`, colorAttr, `red`],
+      [`style`, colorAttr, `green`],
+      [`style`, colorAttr, `blue`],
+      [`style`, colorAttr, `alpha`],
     ]
-    widget.animate(changeProperty(changedProps), startAt, duration, {
+    widget.animate(changeProperty(changedProps.map(c => c.join('.'))), startAt, duration, {
       from: [
-        params.from?.red ?? widget.style.fillColor.red,
-        params.from?.green ?? widget.style.fillColor.green,
-        params.from?.blue ?? widget.style.fillColor.blue,
-        params.from?.alpha ?? widget.style.fillColor.alpha,
+        params.from?.red ?? getByChain(changedProps[0], widget),
+        params.from?.green ?? getByChain(changedProps[1], widget),
+        params.from?.blue ?? getByChain(changedProps[2], widget),
+        params.from?.alpha ?? getByChain(changedProps[3], widget),
       ],
       to: [
         params.to.red,
