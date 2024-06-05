6ea1bd53480c3ca5fdec70c7fce27da59d0d6a4b
jiwangyihao <jwyh@jwyihao.top>
2024-06-05 22:52:43 +0800

    perf(basic): use `ckPath.rewind` to replace `ckPath.reset`

--- packages/basic/src/widgets/arc.ts
+++ packages/basic/src/widgets/arc.ts
@@ -37,11 +37,11 @@ export function createArc(radius: number, from: number, to: number, options?: Ar
       rect.set([-v.value, -v.value, v.value, v.value])
     })
     changed(fromProp, (v: Prop<number>) => {
-      path.path.reset()
+      path.path.rewind()
       path.path.addArc(rect, v.value, toProp.value * path.progress.value)
     })
     changed(toProp, (v: Prop<number>) => {
-      path.path.reset()
+      path.path.rewind()
       path.path.addArc(rect, fromProp.value, v.value * path.progress.value)
     })
 
