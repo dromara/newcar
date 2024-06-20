7d295a3321966eadce1c338afdad1eb78fb2d2d7
Acbox <850625057@qq.com>
2024-06-20 14:50:12 +0800

    chore(cli): fix some problems

--- cli/src/test/main.mjs
+++ cli/src/test/main.mjs
@@ -5,6 +5,7 @@ await useFont('./fonts/bahnschrift.ttf')
 
 const engine = await new CarEngine().init('../../node_modules/canvaskit-wasm/bin/canvaskit.wasm')
 const app = engine.createLocalApp(700, 700)
+app.config.unit = 'frame'
 const logo = new ImageWidget(logoLoaded, {
   style: {
     scaleX: 0.4,
