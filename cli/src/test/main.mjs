65dcc1daf36a452446e5bffc84eccb84cc4324ed
Acbox <850625057@qq.com>
2024-06-20 16:41:51 +0800

    fix: display failed in local mode

--- cli/src/test/main.mjs
+++ cli/src/test/main.mjs
@@ -1,7 +1,6 @@
-import { CarEngine, ImageWidget, Scene, Text, Widget, move, useFont, useImage } from 'newcar'
+import { CarEngine, ImageWidget, Scene, Widget, move, useImage } from 'newcar'
 
 const logoLoaded = await useImage('./assets/newcar.webp')
-await useFont('./fonts/bahnschrift.ttf')
 
 const engine = await new CarEngine().init('../../node_modules/canvaskit-wasm/bin/canvaskit.wasm')
 const app = engine.createLocalApp(700, 700)
@@ -11,17 +10,8 @@ const logo = new ImageWidget(logoLoaded, {
     scaleX: 0.4,
     scaleY: 0.4,
   },
-})
-const text = new Text(['Hello Newcar!'], {
-  y: 600,
-  style: {
-    textAlign: 'center',
-  },
-}).animate(move, 0, 300, {
-  from: [0, 600],
-  to: [-1400, 600],
-})
-const root = new Widget().add(text, logo)
+}).animate(move().withAttr({ duration: 1, to: [400, 400] }))
+const root = new Widget().add(logo)
 const scene = new Scene(root)
 app.checkout(scene)
 
--- examples/main.ts
+++ examples/main.ts
@@ -2,7 +2,7 @@ import * as nc from 'newcar'
 import scene1 from './scene1'
 import timeview from '@newcar/plugin-timeview'
 
-const milestone = document.querySelector('#milestone') as HTMLCanvasElement
+// const milestone = document.querySelector('#milestone') as HTMLCanvasElement
 // window.addEventListener('resize', () => {
 //   milestone.width = window.innerWidth
 //   milestone.height = window.innerHeight
@@ -10,9 +10,9 @@ const milestone = document.querySelector('#milestone') as HTMLCanvasElement
 // window.dispatchEvent(new Event('resize'))
 
 const engine = await new nc.CarEngine().init('./node_modules/canvaskit-wasm/bin/canvaskit.wasm')
-const app = engine.createApp(milestone)
+const app = engine.createLocalApp(1600, 900)
 
 
 app.checkout(scene1)
 
-app.play()
+export default app
