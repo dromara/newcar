166dc864024d19ddaa3716aa054c02974e9badba
sujiaqi <1526394815@qq.com>
2024-08-12 09:32:43 +0800

    fix(100%): fix the demo of the animate

--- docs/content/basic/demos/animation/fadeIn.vue
+++ docs/content/basic/demos/animation/fadeIn.vue
@@ -8,6 +8,19 @@ let play:()=>void
 onMounted(async () => {
   const engine = await new nc.CarEngine().init('https://unpkg.com/canvaskit-wasm@latest/bin/canvaskit.wasm')
   const app = engine.createApp(canvas.value!).setBackgroundColor('transparent')
+  const root = new nc.Circle(100, {
+      x: 103,
+      y: 103,
+      style:{
+        border:true,
+        borderColor:nc.Color.BLACK,
+        borderWidth:3
+      }
+    }
+  )
+  const scene = nc.createScene(root)
+  app.checkout(scene)
+  app.play()
   play=()=> {
     const root = new nc.Circle(100, {
       x: 103,
