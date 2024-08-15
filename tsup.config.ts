01d170cc58365015d956287061aa1daa20660fe9
Acbox liu <850625057@qq.com>
2024-08-15 12:54:15 +0800

    release: v2.0.0-beta.13

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm', 'iife'],
   tsconfig: '../../tsconfig.json',
+  globalName: 'newcar',
   splitting: true,
   clean: true,
   dts: true,
